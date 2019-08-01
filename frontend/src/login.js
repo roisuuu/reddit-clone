463a5b7017c176d3a4aca668c4ed043806b66b4e
z5258844 <z5258844@cse.unsw.edu.au>
2019-08-01 16:26:42 +1000

    beginning work on creating the login modal

--- frontend/src/login.js
+++ frontend/src/login.js
@@ -2,9 +2,25 @@ function loginBasic () {
     const navItems = document.getElementsByClassName("nav-item");
     const loginButton = navItems[1];
     console.log(navItems[1]);
+
+    // function that creates a login modal, with html and css
+
     loginButton.onclick = function () {
+        // summon the modal,
+        // exit if clicked anywhere else
+        /* 
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+        */
         alert("You pressed the login!");     
     };
+};
+
+function createLoginModal() {
+    // SUMMON THE HTML!!!!
 };
 
 loginBasic();
