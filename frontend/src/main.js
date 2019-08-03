b2b905a0b59bc24f664b7411b592ce396b0428cf
z5258844 <z5258844@cse.unsw.edu.au>
2019-08-03 12:54:33 +1000

    created sign up modal

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -11,6 +11,12 @@
 function initApp(apiUrl) {
     // your app initialisation goes here
 
+    initialiseBannerElements();
+
+};
+
+// creates all banner elements: logo, login, search, sign up
+function initialiseBannerElements() {
     // create the header
     const banner = document.createElement('header');
     banner.id = 'nav';
@@ -70,10 +76,11 @@ function initApp(apiUrl) {
     signUpList.appendChild(signUpButt);
     signUpButt.textContent = ('Sign Up');
 
+    createSignUpModal();
     signUpBasic();
+};
 
-}
-
+// basic functionality when the log in button is pressed
 function loginBasic() {
     const navItems = document.getElementsByClassName("nav-item");
     const loginButton = navItems[1];
@@ -83,23 +90,24 @@ function loginBasic() {
 
     loginButton.onclick = function() {
         // summon the modal,
-        // exit if clicked anywhere else
+        // exit if login button is clicked again
         const loginModal = document.getElementById('loginModal')
-
 
         if (loginModal.style.display === "block") {
             loginModal.style.display = "none";
         } else {
             loginModal.style.display = "block";
-            /*
-            window.onclick = function(event) {
-                console.log("im out here");
-                if (event.target !== loginModal) {
-                    console.log("tryna make a change");
-                    loginModal.style.display = "none";
-                }
-            }; */
         }
+
+        // failed function to close modal when clicking outside it
+        /*
+        window.onclick = function(event) {
+            console.log("im out here");
+            if (event.target == loginModal) {
+                console.log("tryna make a change");
+                loginModal.style.display = "none";
+            }
+        }; */
         //alert("You pressed the login!");
     };
 };
@@ -113,6 +121,7 @@ function createLoginModal() {
     document.getElementById('root').appendChild(loginModal);
 
     const title = document.createElement('h1');
+    title.classList.add('login-header');
     title.id = 'loginTitle';
     title.textContent = ('Login');
     loginModal.appendChild(title);
@@ -153,9 +162,75 @@ function signUpBasic() {
     const navItems = document.getElementsByClassName("nav-item");
     const signUpButton = navItems[2];
     console.log(navItems[2]);
-    signUpButton.onclick = function signUpBasic() {
-        alert("You pressed the signup!");
+
+
+    const signUpModal = document.getElementById('signUpModal');
+    console.log(signUpModal);
+    signUpButton.onclick = function() {
+        //alert("You pressed the signup!");
+
+        if (signUpModal.style.display === "block") {
+            signUpModal.style.display = "none";
+        } else {
+            signUpModal.style.display = "block";
+        }
     };
+};
+
+function createSignUpModal() {
+    const signUpModal = document.createElement('div');
+    signUpModal.classList.add('modal-content');
+    signUpModal.classList.add('sign-up-form');
+    signUpModal.id = 'signUpModal';
+    document.getElementById('root').appendChild(signUpModal);
+
+    const title = document.createElement('h1');
+    title.classList.add('sign-up-header');
+    title.id = 'signUpTitle';
+    title.textContent = ('Sign Up');
+    signUpModal.appendChild(title);
+
+    const signUpForm = document.createElement('form');
+    signUpForm.id = 'signUpForm';
+    signUpModal.appendChild(signUpForm);
+    signUpForm.style.textAlign = "center";
+
+    // creates the username form
+    const userName = document.createElement('input');
+    userName.setAttribute('type', "text");
+    userName.setAttribute('placeholder', "Create Username");
+    userName.setAttribute('name', "username");
+    userName.style.width = "80%";
+    // creates the email form
+    const email = document.createElement('input');
+    email.setAttribute('type', "text");
+    email.setAttribute('placeholder', "Email Address");
+    email.setAttribute('name', "email");
+    email.style.width = "80%";
+    // creates the password form
+    const password = document.createElement('input');
+    password.setAttribute('type', "password");
+    password.setAttribute('placeholder', "Password");
+    password.setAttribute('name', "password");
+    password.style.width = "80%";
+    // creates the confirm password form
+    const confirmPassword = document.createElement('input');
+    confirmPassword.setAttribute('type', "password");
+    confirmPassword.setAttribute('placeholder', "Confirm Password");
+    confirmPassword.setAttribute('name', "confirmPassword");
+    confirmPassword.style.width = "80%";
+    // creates a submit button
+    const submitButton = document.createElement('input');
+    submitButton.setAttribute('type', "submit");
+    submitButton.setAttribute('value', "Submit");
+    submitButton.style.width = "80%";
+
+    // appends all of the forms to the loginForm element
+    signUpForm.appendChild(userName);
+    signUpForm.appendChild(email);
+    signUpForm.appendChild(password);
+    signUpForm.appendChild(confirmPassword);
+    signUpForm.appendChild(submitButton);
 };
 
 export default initApp;
